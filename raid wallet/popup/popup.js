253df2d353aa5aa4888f3801324e8158110efd80
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-07-23 00:16:44 +0300

    Update popup.js

--- raid wallet/popup/popup.js
+++ raid wallet/popup/popup.js
@@ -20,38 +20,67 @@ document.addEventListener("DOMContentLoaded", function() {
 	gettingItem = browser.storage.local.get({posterAddress: ""});
 	gettingItem.then(res => {
 		if (res.posterAddress != "" && res.posterAddress != undefined && res.posterAddress != null) {
-			document.getElementById("address").innerHTML = res.posterAddress; document.getElementById("addressDiv").style.display = "block";
+			document.getElementById("address").innerHTML = res.posterAddress;
+			document.getElementById("addressDiv").style.display = "block";
 		} else {
-			document.getElementById("address").innerHTML = "generating...";	document.getElementById("addressDiv").style.display = "block";
+			document.getElementById("address").innerHTML = "generating...";
+			document.getElementById("addressDiv").style.display = "block";
 			browser.storage.onChanged.addListener((changes, area) => {
-				let changedItems = Object.keys(changes);for (let item of changedItems) { if (item == "posterAddress") {document.getElementById("address").innerHTML = changes[item].newValue;} }
+				let changedItems = Object.keys(changes);
+				for (let item of changedItems) {
+					if (item == "posterAddress") {
+						document.getElementById("address").innerHTML = changes[item].newValue;
+					}
+				}
 			});
 		}
 	});
 
-	document.getElementById("setRewardsAddress").addEventListener("click", function(event){event.preventDefault();setRewardsAddress();});
-	document.getElementById('rewardsAddressInput').addEventListener("change", function(event){rewardsAddress = event.target.value;});
-	document.getElementById('rewardsAddressInput').addEventListener("paste", function(event){rewardsAddress = event.target.value;});
-	document.getElementById("editRewardsAddress").addEventListener("click", function(event){event.preventDefault();editRewardsAddress();});
-	document.getElementById("copyAddress").addEventListener("click", function(event){event.preventDefault();copyAddress();});
+	document.getElementById("setRewardsAddress").addEventListener("click", function(event){
+		event.preventDefault();
+		setRewardsAddress();
+	});
+	document.getElementById('rewardsAddressInput').addEventListener("change", function(event){
+		rewardsAddress = event.target.value;
+	});
+	document.getElementById('rewardsAddressInput').addEventListener("paste", function(event){
+		rewardsAddress = event.target.value;
+	});
+	document.getElementById("editRewardsAddress").addEventListener("click", function(event){
+		event.preventDefault();
+		editRewardsAddress();
+	});
+	document.getElementById("copyAddress").addEventListener("click", function(event){
+		event.preventDefault();
+		copyAddress();
+	});
 });
 
 function setRewardsAddress() {
 	rewardsAddress = (rewardsAddress) ? rewardsAddress : document.getElementById("rewardsAddressInput").value; 
 	if (rewardsAddress != undefined) {
-		document.getElementById("rewardsAddressDivSet").style.display = "none"; document.getElementById("rewardsAddressDiv").style.display = "block";
-		document.getElementById("rewardsAddress").innerHTML = rewardsAddress; browser.storage.local.set({rewardsAddress: rewardsAddress});
+		document.getElementById("rewardsAddressDivSet").style.display = "none";
+		document.getElementById("rewardsAddressDiv").style.display = "block";
+		document.getElementById("rewardsAddress").innerHTML = rewardsAddress;
+		browser.storage.local.set({rewardsAddress: rewardsAddress});
 	}
 }
 
 function editRewardsAddress() {
-	document.getElementById("rewardsAddressDivSet").style.display = "block"; document.getElementById("rewardsAddressDiv").style.display = "none";
+	document.getElementById("rewardsAddressDivSet").style.display = "block";
+	document.getElementById("rewardsAddressDiv").style.display = "none";
 	browser.storage.local.get({rewardsAddress: ""}).then(res => {
-		if (res.rewardsAddress != "" && res.rewardsAddress != undefined && res.rewardsAddress != null) {document.getElementById("rewardsAddressInput").value = res.rewardsAddress;}
+		if (res.rewardsAddress != "" && res.rewardsAddress != undefined && res.rewardsAddress != null) {
+			document.getElementById("rewardsAddressInput").value = res.rewardsAddress;
+		}
 	});
 }
 
 function copyAddress() {
-    navigator.clipboard.writeText(document.getElementById("address").innerHTML).then(() =>{}, ()=> {console.log('rip clipboard');});
+    navigator.clipboard.writeText(document.getElementById("address").innerHTML).then(() =>{
+    	console.log('clipboard ok');
+    }, ()=> {
+    	console.log('rip clipboard');
+    });
 }
 
