499d699c44eb15d5aae1690759c24e2e8cd6082e
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-07-27 05:52:05 +0300

    Update popup.js

--- raid wallet/popup/popup.js
+++ raid wallet/popup/popup.js
@@ -43,6 +43,20 @@ document.addEventListener("DOMContentLoaded", function() {
 			document.getElementById("greenResponseSetting").checked = true;
 		}
 	});
+	browser.storage.local.get({newThreadSetting: "on"}).then(res => {
+		if (res.newThreadSetting == "off") {
+			document.getElementById("newThreadSetting").checked = true;
+		}
+	});
+	browser.storage.local.get({newThreadHref: ""}).then(res => {
+		document.getElementById("latestBizThread").href = res.newThreadHref;
+	});
+
+	document.getElementById("latestBizThread").addEventListener("click",(event)=>{if(document.getElementById("latestBizThread").href == ""){event.preventDefault();}});
+	let newThreadSettingCheckbox = document.getElementById("newThreadSetting");
+	newThreadSettingCheckbox.addEventListener("change", function(event){
+		if (newThreadSettingCheckbox.checked) {browser.storage.local.set({newThreadSetting: "off"});} else {browser.storage.local.set({newThreadSetting: ""});}
+	});
 	let timerSettingCheckbox = document.getElementById("timerSetting");
 	let greenResponseSettingCheckbox = document.getElementById("greenResponseSetting");
 	timerSettingCheckbox.addEventListener("change", function(event){
@@ -51,24 +65,12 @@ document.addEventListener("DOMContentLoaded", function() {
 	greenResponseSettingCheckbox.addEventListener("change", function(event){
 		if (greenResponseSettingCheckbox.checked) {browser.storage.local.set({greenResponseSetting: "off"});} else {browser.storage.local.set({greenResponseSetting: "on"});}
 	});
-	document.getElementById("setRewardsAddress").addEventListener("click", function(event){
-		event.preventDefault();
-		setRewardsAddress();
-	});
-	document.getElementById('rewardsAddressInput').addEventListener("change", function(event){
-		rewardsAddress = event.target.value;
-	});
-	document.getElementById('rewardsAddressInput').addEventListener("paste", function(event){
-		rewardsAddress = event.target.value;
-	});
-	document.getElementById("editRewardsAddress").addEventListener("click", function(event){
-		event.preventDefault();
-		editRewardsAddress();
-	});
-	document.getElementById("copyAddress").addEventListener("click", function(event){
-		event.preventDefault();
-		copyAddress();
-	});
+	document.getElementById("faq").addEventListener("click", function(event){event.preventDefault();browser.storage.local.set({faq: true});});
+	document.getElementById("setRewardsAddress").addEventListener("click", function(event){event.preventDefault();setRewardsAddress();});
+	document.getElementById('rewardsAddressInput').addEventListener("change", function(event){rewardsAddress = event.target.value;});
+	document.getElementById('rewardsAddressInput').addEventListener("paste", function(event){rewardsAddress = event.target.value;});
+	document.getElementById("editRewardsAddress").addEventListener("click", function(event){event.preventDefault();editRewardsAddress();});
+	document.getElementById("copyAddress").addEventListener("click", function(event){event.preventDefault();copyAddress();});
 });
 
 function setRewardsAddress() {
