a3eb53976e0abbb14c3a3aa6db63aa0f8b7a9ecd
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-08-21 14:42:05 +0300

    Update popup.js

--- raid wallet/popup/popup.js
+++ raid wallet/popup/popup.js
@@ -6,31 +6,21 @@
  */
 'use strict';
 
-let rewardsAddress;
+let rwrdsddrss;
+function geteid(selector){return document.getElementById(selector);}
 
 document.addEventListener("DOMContentLoaded", function() {
+	geteid("version").textContent = "current version: "+browser.runtime.getManifest().version + " ";
 	browser.storage.local.get({rewardsAddressSet: ""}).then(res => {
 		if (res.rewardsAddressSet != "" && res.rewardsAddressSet != undefined && res.rewardsAddressSet != null) {
-			document.getElementById("rewardsAddress").textContent = res.rewardsAddressSet;
-			document.getElementById("rewardsAddressDivSet").style.display = "none";
-			document.getElementById("rewardsAddressDiv").style.display = "block";
+			geteid("rewardsAddress").textContent = res.rewardsAddressSet; geteid("rewardsAddressDivSet").style.display = "none"; geteid("rewardsAddressDiv").style.display = "inline";
 		}
 	});
 	browser.storage.local.get({posterAddress: ""}).then(res => {
 		if (res.posterAddress != "" && res.posterAddress != undefined && res.posterAddress != null) {
-			document.getElementById("address").textContent = res.posterAddress;
-			document.getElementById("addressDiv").style.display = "block";
+			geteid("address").textContent = res.posterAddress; geteid("addressDiv").style.display = "inline";
 		} else {
-			document.getElementById("address").textContent = "generating...";
-			document.getElementById("addressDiv").style.display = "block";
-			browser.storage.onChanged.addListener((changes, area) => {
-				let changedItems = Object.keys(changes);
-				for (let item of changedItems) {
-					if (item == "posterAddress") {
-						document.getElementById("address").textContent = changes[item].newValue;
-					}
-				}
-			});
+			geteid("address").textContent = "generating..."; geteid("addressDiv").style.display = "inline";
 		}
 	});
 	browser.storage.onChanged.addListener((changes, area) =>{
@@ -38,78 +28,51 @@ document.addEventListener("DOMContentLoaded", function() {
 		for (let item of changedItems) { 
 			if (item == "error") {
 				if (changes[item].newValue == "invalid EVM address, try again"){
-					browser.storage.local.set({error: "invalid EVM address, try again "});
-					document.getElementById("rewardsAddress").textContent = changes[item].newValue;
+					browser.storage.local.set({error: "invalid EVM address, try again "}); geteid("rewardsAddress").textContent = changes[item].newValue;
 				}
+				if (item == "posterAddress") { geteid("address").textContent = changes[item].newValue; }
 			}
 		}
 	});
 	browser.storage.local.get({timerSetting: "off"}).then(res => {
-		if (res.timerSetting == "off") {
-			document.getElementById("timerSetting").checked = true;
-		}
+		if (res.timerSetting == "off") { geteid("timerSetting").checked = true; }
 	});
 	browser.storage.local.get({greenResponseSetting: "on"}).then(res => {
-		if (res.greenResponseSetting == "off") {
-			document.getElementById("greenResponseSetting").checked = true;
-		}
+		if (res.greenResponseSetting == "off") { geteid("greenResponseSetting").checked = true; }
 	});
-	browser.storage.local.get({newThreadSetting: "on"}).then(res => {
-		if (res.newThreadSetting == "off") {
-			document.getElementById("newThreadSetting").checked = true;
-		}
-	});
-	browser.storage.local.get({newThreadHref: "https://boards.4channel.org/biz/catalog"}).then(res => {
-		document.getElementById("latestBizThread").href = res.newThreadHref;
-	});
-
-	document.getElementById("latestBizThread").addEventListener("click",(event)=>{if(document.getElementById("latestBizThread").href == ""){event.preventDefault();}});
-	let newThreadSettingCheckbox = document.getElementById("newThreadSetting");
+	browser.storage.local.get({newThreadSetting: "on"}).then(res => { if (res.newThreadSetting == "off") { geteid("newThreadSetting").checked = true; } });
+	browser.storage.local.get({newThreadHref: "https://boards.4channel.org/biz/catalog"}).then(res => { geteid("latestBizThread").href = res.newThreadHref; });
+	geteid("latestBizThread").addEventListener("click",(event)=>{if(geteid("latestBizThread").href == ""){event.preventDefault();}});
+	let newThreadSettingCheckbox = geteid("newThreadSetting");
 	newThreadSettingCheckbox.addEventListener("change", function(event){
 		if (newThreadSettingCheckbox.checked) {browser.storage.local.set({newThreadSetting: "off"});} else {browser.storage.local.set({newThreadSetting: ""});}
 	});
-	let timerSettingCheckbox = document.getElementById("timerSetting");
-	let greenResponseSettingCheckbox = document.getElementById("greenResponseSetting");
+	let timerSettingCheckbox = geteid("timerSetting"); let greenResponseSettingCheckbox = geteid("greenResponseSetting");
 	timerSettingCheckbox.addEventListener("change", function(event){
 		if (timerSettingCheckbox.checked) {browser.storage.local.set({timerSetting: "off"});} else browser.storage.local.set({timerSetting: "on"});
 	});
 	greenResponseSettingCheckbox.addEventListener("change", function(event){
 		if (greenResponseSettingCheckbox.checked) {browser.storage.local.set({greenResponseSetting: "off"});} else {browser.storage.local.set({greenResponseSetting: "on"});}
 	});
-	document.getElementById("faq").addEventListener("click", function(event){
-		event.preventDefault();browser.storage.local.set({faq: true});
-	});
-	document.getElementById("setRewardsAddress").addEventListener("click", function(event){event.preventDefault();setRewardsAddress();});
-	document.getElementById('rewardsAddressInput').addEventListener("change", function(event){rewardsAddress = event.target.value;});
-	document.getElementById('rewardsAddressInput').addEventListener("paste", function(event){rewardsAddress = event.target.value;});
-	document.getElementById("editRewardsAddress").addEventListener("click", function(event){event.preventDefault();editRewardsAddress();});
-	document.getElementById("copyAddress").addEventListener("click", function(event){event.preventDefault();copyAddress();});
+	geteid("setRewardsAddress").addEventListener("click", function(event){event.preventDefault();setRewardsAddress();});
+	geteid('rewardsAddressInput').addEventListener("change", function(event){rwrdsddrss = event.target.value;});
+	geteid('rewardsAddressInput').addEventListener("paste", function(event){rwrdsddrss = event.target.value;});
+	geteid("editRewardsAddress").addEventListener("click", function(event){event.preventDefault();editRewardsAddress();});
+	geteid("options").addEventListener("click", function(e){ e.preventDefault(); let u = browser.runtime.getURL("popup/home.html"); window.open(u,'_blank'); });
+	geteid("version").addEventListener("click", function(e){ e.preventDefault(); window.open("https://addons.mozilla.org/en-US/firefox/addon/aletheo-wallet/versions/",'_blank'); });
 });
 
 function setRewardsAddress() {
-	rewardsAddress = (rewardsAddress) ? rewardsAddress : document.getElementById("rewardsAddressInput").value; 
-	if (rewardsAddress != undefined) {
-		document.getElementById("rewardsAddressDivSet").style.display = "none";
-		document.getElementById("rewardsAddressDiv").style.display = "block";
-		document.getElementById("rewardsAddress").textContent = rewardsAddress;
-		browser.storage.local.set({rewardsAddress: rewardsAddress});
+	rwrdsddrss = (rwrdsddrss) ? rwrdsddrss : geteid("rewardsAddressInput").value; 
+	if (rwrdsddrss != undefined) {
+		geteid("rewardsAddressDivSet").style.display = "none"; geteid("rewardsAddressDiv").style.display = "inline"; geteid("rewardsAddress").textContent = rwrdsddrss; 
+		browser.storage.local.set({rewardsAddress: rwrdsddrss});
 	}
 }
 
 function editRewardsAddress() {
-	document.getElementById("rewardsAddressDivSet").style.display = "block";
-	document.getElementById("rewardsAddressDiv").style.display = "none";
+	geteid("rewardsAddressDivSet").style.display = "inline"; geteid("rewardsAddressDiv").style.display = "none";
 	browser.storage.local.get({rewardsAddressSet: ""}).then(res => {
-		if (res.rewardsAddressSet != "" && res.rewardsAddressSet != undefined && res.rewardsAddressSet != null) {
-			document.getElementById("rewardsAddressInput").value = res.rewardsAddressSet;
-		}
+		if (res.rewardsAddressSet != "" && res.rewardsAddressSet != undefined && res.rewardsAddressSet != null) { geteid("rewardsAddressInput").value = res.rewardsAddressSet; }
 	});
-}
-
-function copyAddress() {
-    navigator.clipboard.writeText(document.getElementById("address").textContent).then(() =>{
-    	console.log('clipboard ok');
-    }, ()=> {
-    	console.log('rip clipboard');
-    });
 }
