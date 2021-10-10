2c094d6f2c0b1fa93d00e209ea5b164b93221b0d
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-10-10 09:03:24 +0300

    Update EmptyContract.sol

--- contracts/mainnet/EmptyContract.sol
+++ contracts/mainnet/EmptyContract.sol
@@ -2,5 +2,8 @@
 pragma solidity ^0.7.6;
 
 contract EmptyContract {
-	// this completely fixes overflow in treasury even before the protocol upgrade
+    function init() public {
+        // this completely fixes overflow in treasury even before the protocol upgrade
+	    // this must be locked, so that this contract can't suddenly be upgraded, and claim something maliciously
+    }
 }
