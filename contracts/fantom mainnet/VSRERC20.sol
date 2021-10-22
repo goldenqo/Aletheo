7366ac244f4d67203abc222e35b097de8f2d4c9a
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-10-22 20:21:57 +0300

    Update VSRERC20.sol

--- contracts/fantom mainnet/VSRERC20.sol
+++ contracts/fantom mainnet/VSRERC20.sol
@@ -11,7 +11,7 @@ pragma solidity ^0.7.6;
 
 interface I{function genesisBlock() external view returns(uint);}
 
-contract VSRERC {
+contract eERC {
 	event Transfer(address indexed from, address indexed to, uint value);
 	event Approval(address indexed owner, address indexed spender, uint value);
 	event BulkTransfer(address indexed from, address[] indexed recipients, uint[] amounts);
@@ -22,41 +22,49 @@ contract VSRERC {
 	string private _name;
 	string private _symbol;
 	bool private _init;
-
+    address private _treasury;
+    address private _founding;
+    address private _staking;
+    
 	function init() public {
-		require(_init == false);
-		_init = true;
-		_name = "Aletheo";
-		_symbol = "LET";
-		_balances[0xe74930ff5d32DB0FF6F2Bd2b7d8c30E4F877d9bb] = 1e24;//founding event
-		_balances[0x61218748bE0bB61e3675b73bA7b1A037a808f095] = 9e24;//treasury
+	//	require(_init == false && msg.sender == 0x3F22EA01e31c6D9b208cd6E95F9B4c74F3C9AFa6);
+	    require(_init == false && msg.sender == 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+		_init = true; _name = "Aletheo"; _symbol = "LET";
+		_treasury = 0x2D9F853F1a71D0635E64FcC4779269A05BccE2E2;
+		_founding = 0x2D9F853F1a71D0635E64FcC4779269A05BccE2E2;
+		_staking = 0x2D9F853F1a71D0635E64FcC4779269A05BccE2E2;
+		_balances[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] = 6e24;
+	//	_balances[0x3F22EA01e31c6D9b208cd6E95F9B4c74F3C9AFa6] = 6e24;//treasury
 	}
+	
 	function name() public view returns (string memory) {return _name;}
 	function symbol() public view returns (string memory) {return _symbol;}
-	function totalSupply() public view returns (uint) {return 10e24-_balances[0x61218748bE0bB61e3675b73bA7b1A037a808f095];}//subtract balance of treasury
+	function totalSupply() public view returns (uint) {return 6e24-_balances[_treasury];}//subtract balance of treasury
 	function decimals() public pure returns (uint) {return 18;}
 	function balanceOf(address a) public view returns (uint) {return _balances[a];}
 	function transfer(address recipient, uint amount) public returns (bool) {_transfer(msg.sender, recipient, amount);return true;}
 	function disallow(address spender) public returns (bool) {delete _allowances[msg.sender][spender];emit Approval(msg.sender, spender, 0);return true;}
 
-	function approve(address spender, uint amount) public returns (bool) { // hardcoded spooky ftm uniswapv2 router 02, transfer helper library
-		if (spender == 0xF491e7B69E4244ad4002BC14e878a34207E38c29) {emit Approval(msg.sender, spender, 2**256 - 1);return true;}
+	function approve(address spender, uint amount) public returns (bool) { // hardcoded uniswapv2 router 02, transfer helper library, also spirit
+		if (spender == 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D||spender == 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D||spender == _staking) {emit Approval(msg.sender, spender, 2**256 - 1);return true;}
 		else {_allowances[msg.sender][spender] = true;emit Approval(msg.sender, spender, 2**256 - 1);return true;}
 	}
 
-	function allowance(address owner, address spender) public view returns (uint) { // hardcoded spooky ftm uniswapv2 router 02, transfer helper library
-		if (spender == 0xF491e7B69E4244ad4002BC14e878a34207E38c29||_allowances[owner][spender] == true) {return 2**256 - 1;} else {return 0;}
+	function allowance(address owner, address spender) public view returns (uint) { // uniswapv2 router 02, transfer helper library
+		if (spender == 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D||spender == 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D||spender == _staking||_allowances[owner][spender] == true) {return 2**256 - 1;} else {return 0;}//ADD STAKING
 	}
 
-	function transferFrom(address sender, address recipient, uint amount) public returns (bool) { // hardcoded spooky ftm uniswapv2 router 02, transfer helper library
-		require(msg.sender == 0xF491e7B69E4244ad4002BC14e878a34207E38c29 || _allowances[sender][msg.sender] == true);_transfer(sender, recipient, amount);return true;
+	function transferFrom(address sender, address recipient, uint amount) public returns (bool) { // uniswapv2 router 02, transfer helper library
+		require(msg.sender == 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D||msg.sender == 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D||msg.sender == _staking||_allowances[sender][msg.sender] == true);
+		_transfer(sender, recipient, amount);return true;
 	}
 
 	function _transfer(address sender, address recipient, uint amount) internal {
-		require(sender != address(0));
+	    	uint senderBalance = _balances[sender];
+		require(sender != address(0)&&senderBalance >= amount);
+		uint treasuryShare = amount/100;
 		_beforeTokenTransfer(sender, amount);
-		uint senderBalance = _balances[sender];
-		require(senderBalance >= amount);
+		if(recipient!=_staking){ amount -= treasuryShare; _balances[_treasury] += treasuryShare; }
 		_balances[sender] = senderBalance - amount;
 		_balances[recipient] += amount;
 		emit Transfer(sender, recipient, amount);
@@ -66,20 +74,18 @@ contract VSRERC {
 		require(recipients.length == amounts.length && amounts.length < 100,"human error");
 		uint senderBalance = _balances[msg.sender]; uint total;
 		for(uint i = 0;i<amounts.length;i++) {total += amounts[i];_balances[recipients[i]] += amounts[i];}
-		require(senderBalance >= total);
-		if (msg.sender == 0x61218748bE0bB61e3675b73bA7b1A037a808f095) {_beforeTokenTransfer(msg.sender, total);}//treasury
-		_balances[msg.sender] = senderBalance - total;
-		emit BulkTransfer(msg.sender, recipients, amounts);
-		return true;
+		require(senderBalance >= total,"balance is low");
+		if (msg.sender == _treasury) {_beforeTokenTransfer(msg.sender, total);}//treasury
+		_balances[msg.sender] = senderBalance - total; emit BulkTransfer(msg.sender, recipients, amounts); return true;
 	}
 
 	function _beforeTokenTransfer(address from, uint amount) internal view {
-		if(from == 0x61218748bE0bB61e3675b73bA7b1A037a808f095) {//from treasury
-			uint genesisBlock = I(0xe74930ff5d32DB0FF6F2Bd2b7d8c30E4F877d9bb).genesisBlock();//founding
+		if(from == _treasury) {//from treasury
+			uint genesisBlock = I(_founding).genesisBlock();
 			require(genesisBlock != 0);
-			uint treasury = _balances[0x61218748bE0bB61e3675b73bA7b1A037a808f095]; //treasury
-			uint withd =  9e24 - treasury;
-			uint allowed = (block.number - genesisBlock)*28e15 - withd;
+			uint treasury = _balances[_treasury]; //treasury
+			uint withd =  5e24 - treasury;
+			uint allowed = (block.number - genesisBlock)*31e15 - withd;
 			require(amount <= allowed && amount <= treasury);
 		}
 	}
